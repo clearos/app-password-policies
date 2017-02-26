666122f550ca5b2572549a58937f2563d84aa60e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:31:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'Дансны бичнэ';
+$lang['password_policies_app_description'] = 'Нууц үг Бодлогын апп нарийн тааруулж хэрэглэгчийн нууц үгийг бодлого журмын ашиглаж байна. Энэ нь таны шалгуурыг хангасан нууц идэвхжүүлдэг бөгөөд таны систем илүү аюулгүй болгохын тулд тусалдаг.';
+$lang['password_policies_app_name'] = 'Нууц үг Бодлого';
+$lang['password_policies_history_size'] = 'түүх Хэмжээ';
+$lang['password_policies_maximum_password_age'] = 'Хамгийн их Нууц үг Нас';
+$lang['password_policies_minimum_password_age'] = 'Хамгийн бага Нууц үг Нас';
+$lang['password_policies_minimum_password_length'] = 'Хамгийн бага Нууц үг Урт';
+$lang['password_policies_modify_any_time'] = 'Ямарч цаг өөрчлөх';
+$lang['password_policies_no_expire'] = 'Ямар ч дуусна';
+$lang['password_policies_no_history'] = 'Ямар ч түүх';
