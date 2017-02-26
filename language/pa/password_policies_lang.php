20312fea575e0e390411de722fd97cd63e2f05ab
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:32:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'ਖਾਤਾ';
+$lang['password_policies_app_description'] = 'ਪਾਸਵਰਡ ਨੀਤੀ ਐਪਲੀਕੇਸ਼ ਨੂੰ ਵਧੀਆ ਉਪਭੋਗੀ ਨੂੰ ਟਿਊਨ ਪਾਸਵਰਡ ਨੀਤੀ ਨੂੰ ਨਿਯਮ ਕਰਨ ਲਈ ਵਰਤਿਆ ਗਿਆ ਹੈ. ਇਹ ਗੁਪਤ-ਕੋਡ ਹੈ, ਜੋ ਕਿ ਆਪਣੇ ਮਾਪਦੰਡ ਨੂੰ ਪੂਰਾ ਲਾਗੂ ਹੈ ਅਤੇ ਆਪਣੇ ਸਿਸਟਮ ਨੂੰ ਹੋਰ ਵੀ ਸੁਰੱਖਿਅਤ ਕਰਨ ਲਈ ਮਦਦ ਕਰਦਾ ਹੈ.';
+$lang['password_policies_app_name'] = 'ਪਾਸਵਰਡ ਡਰਾਇਰ';
+$lang['password_policies_history_size'] = 'ਇਤਿਹਾਸ ਦਾ ਸਾਈਜ਼';
+$lang['password_policies_maximum_password_age'] = 'ਵੱਧ ਪਾਸਵਰਡ ਉਮਰ';
+$lang['password_policies_minimum_password_age'] = 'ਘੱਟੋ ਪਾਸਵਰਡ ਉਮਰ';
+$lang['password_policies_minimum_password_length'] = 'ਘੱਟੋ-ਘੱਟ ਪਾਸਵਰਡ ਲੰਬਾਈ';
+$lang['password_policies_modify_any_time'] = 'ਤਬਦੀਲੀ ਕਿਸੇ ਵੀ ਵੇਲੇ';
+$lang['password_policies_no_expire'] = 'ਕੋਈ ਮਿਆਦ';
+$lang['password_policies_no_history'] = 'ਕੋਈ ਇਤਿਹਾਸ';
