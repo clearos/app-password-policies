50877551666c0926c8443ca3eb951be2a8f4df26
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:26:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'konto blokeeringu';
+$lang['password_policies_app_description'] = 'Parooli Policy app kasutatakse häälestada kasutaja parooli reeglitest. See sunnib paroole, mis vastavad teie kriteeriumidele ning aitab muuta teie süsteemi turvalisemaks.';
+$lang['password_policies_app_name'] = 'parool poliitika';
+$lang['password_policies_history_size'] = 'Ajalugu Suurus';
+$lang['password_policies_maximum_password_age'] = 'Maksimaalne Salasõna Age';
+$lang['password_policies_minimum_password_age'] = 'Minimaalne Salasõna Age';
+$lang['password_policies_minimum_password_length'] = 'Minimaalne Salasõna pikkus';
+$lang['password_policies_modify_any_time'] = 'Muuta iga aeg';
+$lang['password_policies_no_expire'] = 'ei aegub';
+$lang['password_policies_no_history'] = 'ei History';
