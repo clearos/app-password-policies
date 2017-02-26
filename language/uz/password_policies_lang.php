44dda02af3395ed9a96d38b770f142b086fb2363
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:35:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'Account Lockout';
+$lang['password_policies_app_description'] = 'Parol siyosati ilova nozik tune foydalanuvchi parol siyosati qoidalariga ishlatiladi. Bu sizning mezonlariga javob parollarini ta&#39;minlaydi va tizim yanada xavfsiz qilish yordam beradi.';
+$lang['password_policies_app_name'] = 'Parol siyosat';
+$lang['password_policies_history_size'] = 'Tarix hajmi';
+$lang['password_policies_maximum_password_age'] = 'Maksimal Parol Yosh';
+$lang['password_policies_minimum_password_age'] = 'Minimal Parol Yosh';
+$lang['password_policies_minimum_password_length'] = 'Minimal Parol uzunligi';
+$lang['password_policies_modify_any_time'] = 'Har qanday vaqt o&#39;zgartirish';
+$lang['password_policies_no_expire'] = 'No, Yakunlaganingizdan';
+$lang['password_policies_no_history'] = 'No tarixi';
