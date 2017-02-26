0d8d9f1390553242fdc0f116ec64acedcbf38741
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:31:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'kont lockout';
+$lang['password_policies_app_description'] = 'Il-app Politika Password tintuża għal regoli ta &#39;politika password multa utent tixgħel. Din il tinforza passwords li jilħqu kriterji tiegħek u tgħin biex tagħmel is-sistema tiegħek aktar siguri.';
+$lang['password_policies_app_name'] = 'Politiki Password';
+$lang['password_policies_history_size'] = 'Daqs storja';
+$lang['password_policies_maximum_password_age'] = 'Età massima Password';
+$lang['password_policies_minimum_password_age'] = 'Minimu Password Età';
+$lang['password_policies_minimum_password_length'] = 'Password Tul minimu';
+$lang['password_policies_modify_any_time'] = 'Timmodifika Kwalunkwe Ħin';
+$lang['password_policies_no_expire'] = 'Nru jiskadu';
+$lang['password_policies_no_history'] = 'Nru Storja';
