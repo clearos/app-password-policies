beb22ba392f0771906620d7ce8d396809cede517
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:27:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gl/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'Bloqueo da conta';
+$lang['password_policies_app_description'] = 'A aplicación da política de contrasinal é usado para regras de política de contrasinal do usuario Axuste fino. Isto reforza os contrasinais que atenden aos seus criterios e axuda a facer o sistema máis seguro.';
+$lang['password_policies_app_name'] = 'Directivas de contrasinal';
+$lang['password_policies_history_size'] = 'historial Tamaño';
+$lang['password_policies_maximum_password_age'] = 'Tempo máximo de contrasinal';
+$lang['password_policies_minimum_password_age'] = 'Minimum Password Age';
+$lang['password_policies_minimum_password_length'] = 'Contrasinal lonxitude mínima';
+$lang['password_policies_modify_any_time'] = 'Modificar calquera hora';
+$lang['password_policies_no_expire'] = 'sen Expira';
+$lang['password_policies_no_history'] = 'no History';
