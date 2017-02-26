3f966a94a0efa8af3d45147982d27a1a34029cdb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:29:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'គណនីជាប់';
+$lang['password_policies_app_description'] = 'កម្មវិធីគោលនយោបាយពាក្យសម្ងាត់គឺត្រូវបានប្រើទៅតាមក្បួនច្បាប់គោលនយោបាយពាក្យសម្ងាត់អ្នកប្រើបទភ្លេងល្អ។ នេះអនុវត្តពាក្យសម្ងាត់ដែលមានលក្ខណៈរបស់អ្នកនិងជួយធ្វើឱ្យប្រព័ន្ធរបស់អ្នកកាន់តែមានសុវត្ថិភាព។';
+$lang['password_policies_app_name'] = 'គោលការណ៍ពាក្យសម្ងាត់';
+$lang['password_policies_history_size'] = 'ទំហំប្រវត្តិសាស្រ្ត';
+$lang['password_policies_maximum_password_age'] = 'អាយុពាក្យសម្ងាត់អតិបរមា';
+$lang['password_policies_minimum_password_age'] = 'អាយុពាក្យសម្ងាត់អប្បបរមា';
+$lang['password_policies_minimum_password_length'] = 'ប្រវែងពាក្យសម្ងាត់អប្បបរមា';
+$lang['password_policies_modify_any_time'] = 'កែប្រែនៅពេលណាមួយ';
+$lang['password_policies_no_expire'] = 'គ្មានការផុតកំណត់';
+$lang['password_policies_no_history'] = 'គ្មានប្រវត្តិ';
