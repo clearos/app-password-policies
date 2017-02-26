40d6b7877f7f6aeaeac5757bdddeca3989a99b15
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:35:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'hesap Kilitleme';
+$lang['password_policies_app_description'] = 'Şifre Politikası uygulaması ince ayar kullanıcı şifresi politikası kurallarına kullanılır. Bu kriterlere uygun şifreleri zorlar ve sisteminizi daha güvenli hale getirmeye yardımcı olur.';
+$lang['password_policies_app_name'] = 'Şifre Politikaları';
+$lang['password_policies_history_size'] = 'Geçmiş Boyutu';
+$lang['password_policies_maximum_password_age'] = 'Maksimum Şifre Yaş';
+$lang['password_policies_minimum_password_age'] = 'Asgari Şifre Yaş';
+$lang['password_policies_minimum_password_length'] = 'Asgari Parola Uzunluğu';
+$lang['password_policies_modify_any_time'] = 'Any Time değiştirme';
+$lang['password_policies_no_expire'] = 'Hayır Geçerlilik';
+$lang['password_policies_no_history'] = 'hiçbir Tarih';
