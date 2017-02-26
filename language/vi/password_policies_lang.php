c2840ee01f5644004166302c36a43dda52a7db35
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:36:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'Account Lockout';
+$lang['password_policies_app_description'] = 'Các ứng dụng Chính sách mật khẩu được sử dụng để quy định chính sách mật khẩu người dùng tinh chỉnh. Này thực thi mật khẩu đáp ứng tiêu chí của bạn và giúp cho hệ thống của bạn an toàn hơn.';
+$lang['password_policies_app_name'] = 'Chính sách mật khẩu';
+$lang['password_policies_history_size'] = 'Kích lịch sử';
+$lang['password_policies_maximum_password_age'] = 'Password Age tối đa';
+$lang['password_policies_minimum_password_age'] = 'Minimum Password Age';
+$lang['password_policies_minimum_password_length'] = 'Minimum Password Length';
+$lang['password_policies_modify_any_time'] = 'Sửa đổi Bất kỳ Thời gian';
+$lang['password_policies_no_expire'] = 'không Hết Hạn';
+$lang['password_policies_no_history'] = 'không Lịch sử';
