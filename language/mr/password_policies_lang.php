f41ade36e177fdd721c399963b61323b60b1447b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:31:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'खाते लॉकआउट';
+$lang['password_policies_app_description'] = 'पासवर्ड धोरण अॅप जुळविण्यासाठी वापरकर्ता संकेतशब्द धोरण नियम केला जातो. हे आपल्या निकष पूर्ण संकेतशब्द अंमलबजावणी आणि तुमची प्रणाली अधिक सुरक्षित करण्यासाठी मदत करते.';
+$lang['password_policies_app_name'] = 'पासवर्ड धोरणे';
+$lang['password_policies_history_size'] = 'इतिहास आकार';
+$lang['password_policies_maximum_password_age'] = 'जास्तीत जास्त पासवर्ड वय';
+$lang['password_policies_minimum_password_age'] = 'किमान पासवर्ड वय';
+$lang['password_policies_minimum_password_length'] = 'किमान संकेतशब्द लांबी';
+$lang['password_policies_modify_any_time'] = 'सुधारित टाईम';
+$lang['password_policies_no_expire'] = 'संपणे नाही';
+$lang['password_policies_no_history'] = 'इतिहास नाही';
