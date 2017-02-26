591e65a30ad1437f9025733b0223c9c064daa031
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:27:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/de/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'Kontosperrungs';
+$lang['password_policies_app_description'] = 'Die Kennwortrichtlinie App wird zur Feinabstimmung Benutzer Passwort-Policy-Regeln verwendet. Dies erzwingt Kennwörter, die Ihre Kriterien erfüllen und hilft Ihr System sicherer zu machen.';
+$lang['password_policies_app_name'] = 'Kennwortrichtlinien';
+$lang['password_policies_history_size'] = 'Geschichte Größe';
+$lang['password_policies_maximum_password_age'] = 'Maximales Kennwortalter';
+$lang['password_policies_minimum_password_age'] = 'Minimales Kennwortalter';
+$lang['password_policies_minimum_password_length'] = 'Minimale Kennwortlänge';
+$lang['password_policies_modify_any_time'] = 'Ändern Jederzeit';
+$lang['password_policies_no_expire'] = 'Kein ablaufen';
+$lang['password_policies_no_history'] = 'Keine Geschichte';
