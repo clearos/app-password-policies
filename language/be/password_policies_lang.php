e8153caff3199bf7e41375858fb369993b58ca9e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:25:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/be/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'блакавання ўліковага запісу';
+$lang['password_policies_app_description'] = 'Дадатак Палітыка пароляў выкарыстоўваецца для тонкіх правілаў палітыкі пароляў карыстальнікаў наладзіцца. Гэта забяспечвае захаванне паролі, якія адказваюць вашым крытэрам і дапамагае зрабіць вашу сістэму больш бяспечнай.';
+$lang['password_policies_app_name'] = 'палітыкі пароляў';
+$lang['password_policies_history_size'] = 'гісторыя Памер';
+$lang['password_policies_maximum_password_age'] = 'Максімальны тэрмін дзеяння пароля';
+$lang['password_policies_minimum_password_age'] = 'Мінімальны тэрмін дзеяння пароля';
+$lang['password_policies_minimum_password_length'] = 'Мінімальная даўжыня пароля';
+$lang['password_policies_modify_any_time'] = 'Змяніць любы час';
+$lang['password_policies_no_expire'] = 'няма Expire';
+$lang['password_policies_no_history'] = 'няма гісторыі';
