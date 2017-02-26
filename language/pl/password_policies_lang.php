27199e41df7602427a2e4ca311a9fcb5da58d2e1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:32:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'blokady konta';
+$lang['password_policies_app_description'] = 'Aplikacja Zasady haseł służy do drobnych zasad polityki hasło użytkownika dostroić. To wymusza hasła, które spełniają te kryteria i pomaga uczynić system bardziej bezpieczne.';
+$lang['password_policies_app_name'] = 'Polityk Hasło';
+$lang['password_policies_history_size'] = 'Historia Rozmiar';
+$lang['password_policies_maximum_password_age'] = 'Maksymalny wiek hasła';
+$lang['password_policies_minimum_password_age'] = 'Minimalny wiek hasła';
+$lang['password_policies_minimum_password_length'] = 'Minimalna długość hasła';
+$lang['password_policies_modify_any_time'] = 'Modyfikacja Any Time';
+$lang['password_policies_no_expire'] = 'Nie Wygasnąć';
+$lang['password_policies_no_history'] = 'Brak historii';
