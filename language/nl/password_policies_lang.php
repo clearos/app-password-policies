6d73372314c3e1eb21f0e6c9d75404df17d52217
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:32:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'account Lockout';
+$lang['password_policies_app_description'] = 'De Password Policy-app wordt gebruikt om fijne wachtwoordbeleid regels tune gebruiker. Dit dwingt wachtwoorden die voldoen aan uw criteria en helpt om uw systeem veiliger te maken.';
+$lang['password_policies_app_name'] = 'Password Policies';
+$lang['password_policies_history_size'] = 'geschiedenis Size';
+$lang['password_policies_maximum_password_age'] = 'Maximum Password Age';
+$lang['password_policies_minimum_password_age'] = 'Minimum Password Age';
+$lang['password_policies_minimum_password_length'] = 'Minimum Password Length';
+$lang['password_policies_modify_any_time'] = 'Wijzigen Any Time';
+$lang['password_policies_no_expire'] = 'geen verlopen';
+$lang['password_policies_no_history'] = 'geen Geschiedenis';
