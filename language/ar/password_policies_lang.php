73a7da7f5cf43707d76cd0d32581af37e7e8b697
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:24:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'تأمين الحساب';
+$lang['password_policies_app_description'] = 'يستخدم التطبيق كلمة المرور سياسة لقواعد سياسة كلمة المرور غرامة المستعمل لحن. هذا يفرض كلمات المرور التي تلبي المعايير الخاصة بك، ويساعد على جعل النظام أكثر أمنا.';
+$lang['password_policies_app_name'] = 'سياسة كلمة المرور';
+$lang['password_policies_history_size'] = 'التاريخ الحجم';
+$lang['password_policies_maximum_password_age'] = 'الحد الأقصى لعمر كلمة المرور';
+$lang['password_policies_minimum_password_age'] = 'الحد الأدنى لعمر كلمة المرور';
+$lang['password_policies_minimum_password_length'] = 'الحد الأدنى طول كلمة المرور';
+$lang['password_policies_modify_any_time'] = 'تعديل أي وقت';
+$lang['password_policies_no_expire'] = 'لا الصلاحية';
+$lang['password_policies_no_history'] = 'لا يوجد شئ';
