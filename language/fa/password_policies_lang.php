3faa0bca80f631f2bf798d3541bf525a2c646ad5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:32:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'حساب تحریم';
+$lang['password_policies_app_description'] = 'برنامه سیاست رمز عبور به قواعد سیاست رمز عبور کاربر لحن خوب استفاده می شود. این اجرا رمزهای عبور که معیارهای خود را و کمک می کند تا سیستم خود را امن تر است.';
+$lang['password_policies_app_name'] = 'سیاست های رمز عبور';
+$lang['password_policies_history_size'] = 'تاریخچه اندازه';
+$lang['password_policies_maximum_password_age'] = 'حداکثر سن رمز عبور';
+$lang['password_policies_minimum_password_age'] = 'حداقل سن رمز عبور';
+$lang['password_policies_minimum_password_length'] = 'حداقل طول رمز عبور';
+$lang['password_policies_modify_any_time'] = 'تغییر هر زمان';
+$lang['password_policies_no_expire'] = 'بدون انقضا';
+$lang['password_policies_no_history'] = 'بدون سابقه';
