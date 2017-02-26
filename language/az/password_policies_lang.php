984995a286315a5d378b1366ffc02b38453a7a7f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:24:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'hesab Lockout';
+$lang['password_policies_app_description'] = 'Password Policy app gözəl tune istifadəçi parol siyasət qaydaları üçün istifadə olunur. Bu meyarlara cavab parol tətbiq və sistem daha təhlükəsiz etmək üçün kömək edir.';
+$lang['password_policies_app_name'] = 'Şifrə siyasəti';
+$lang['password_policies_history_size'] = 'Tarix Size';
+$lang['password_policies_maximum_password_age'] = 'Maximum Password Age';
+$lang['password_policies_minimum_password_age'] = 'Minimum Password Age';
+$lang['password_policies_minimum_password_length'] = 'Minimum Password Length';
+$lang['password_policies_modify_any_time'] = 'Hər hansı bir vaxt dəyişə';
+$lang['password_policies_no_expire'] = 'No olmadığını';
+$lang['password_policies_no_history'] = 'No Tarix';
