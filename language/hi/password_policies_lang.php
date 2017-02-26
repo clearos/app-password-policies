0280d5da1c3549e4db20d951d7de8f9de7fcbe5f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:28:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'खाते में तालाबंदी';
+$lang['password_policies_app_description'] = 'पासवर्ड नीति एप्लिकेशन ठीक उपयोगकर्ता धुन पासवर्ड नीति नियमों में प्रयोग किया जाता है। यह पासवर्ड है कि आपके मानदंडों को पूरा लागू करता है और अपने सिस्टम को और अधिक सुरक्षित बनाने के लिए मदद करता है।';
+$lang['password_policies_app_name'] = 'पासवर्ड नीतियाँ';
+$lang['password_policies_history_size'] = 'इतिहास का आकार';
+$lang['password_policies_maximum_password_age'] = 'अधिकतम पासवर्ड उम्र';
+$lang['password_policies_minimum_password_age'] = 'न्यूनतम पासवर्ड उम्र';
+$lang['password_policies_minimum_password_length'] = 'पासवर्ड की न्यूनतम लंबाई';
+$lang['password_policies_modify_any_time'] = 'संशोधित किसी भी समय';
+$lang['password_policies_no_expire'] = 'कोई समय सीमा समाप्त';
+$lang['password_policies_no_history'] = 'कोई इतिहास';
