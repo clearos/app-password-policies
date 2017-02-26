e48a91449f4c3dd973171e59f260b87dea68fc2e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:36:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'cyfrif Streic Fawr y';
+$lang['password_policies_app_description'] = 'Mae&#39;r app Polisi Cyfrinair cael ei ddefnyddio i reolau polisi cyfrinair defnyddiwr fireinio. Mae hyn yn gorfodi cyfrineiriau sy&#39;n bodloni eich meini prawf ac yn helpu i wneud eich system yn fwy diogel.';
+$lang['password_policies_app_name'] = 'Polisïau cyfrinair';
+$lang['password_policies_history_size'] = 'Maint hanes';
+$lang['password_policies_maximum_password_age'] = 'Cyfrinair Uchafswm Oedran';
+$lang['password_policies_minimum_password_age'] = 'Cyfrinair Isafswm Oedran';
+$lang['password_policies_minimum_password_length'] = 'Isafswm Hyd Cyfrinair';
+$lang['password_policies_modify_any_time'] = 'Addasu Unrhyw Adeg';
+$lang['password_policies_no_expire'] = 'Dim Darfod';
+$lang['password_policies_no_history'] = 'Dim History';
