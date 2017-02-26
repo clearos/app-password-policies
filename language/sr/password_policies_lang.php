c749b7fba2d8024e720d5b80955c0a52f5eba9e5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:33:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'account Лоцкоут';
+$lang['password_policies_app_description'] = 'Апликација Смернице лозинке се користи за фине правила подешавање корисничких лозинка политике. Ово спроводи лозинке које задовољавају критеријум и помаже да се ваш систем сигурнијим.';
+$lang['password_policies_app_name'] = 'пассворд Политике';
+$lang['password_policies_history_size'] = 'istorija величина';
+$lang['password_policies_maximum_password_age'] = 'Максимални Лозинка Године';
+$lang['password_policies_minimum_password_age'] = 'Минимална Лозинка Године';
+$lang['password_policies_minimum_password_length'] = 'Минимална Шифра Дужина';
+$lang['password_policies_modify_any_time'] = 'Модификује било време';
+$lang['password_policies_no_expire'] = 'но Истиче';
+$lang['password_policies_no_history'] = 'но Историја';
