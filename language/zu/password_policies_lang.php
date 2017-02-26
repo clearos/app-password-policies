09293c32405b5b38465edfe35eb271ae088b04b7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:36:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'impelasikhathi ye-akhawunti';
+$lang['password_policies_app_description'] = 'Uhlelo lokusebenza Iphasiwedi yemfihlo isetshenziselwa emihle kutsha umsebenzisi imithetho inqubomgomo iphasiwedi. Lokhu isebenzisa owayo amaphasiwedi ukuthi ukuhlangabezana nezidingo zakho futhi kusiza ukwenza uhlelo lwakho kulondeke kakhudlwana.';
+$lang['password_policies_app_name'] = 'Izinqubomgomo Iphasiwedi';
+$lang['password_policies_history_size'] = 'Umlando Usayizi';
+$lang['password_policies_maximum_password_age'] = 'Maximum Iphasiwedi Age';
+$lang['password_policies_minimum_password_age'] = 'Minimum Iphasiwedi Age';
+$lang['password_policies_minimum_password_length'] = 'Iphasiwedi Ubude Minimum';
+$lang['password_policies_modify_any_time'] = 'Lungisa yisiphi isikhathi';
+$lang['password_policies_no_expire'] = 'Ayikho yisikhathi';
+$lang['password_policies_no_history'] = 'Ayikho Umlando';
