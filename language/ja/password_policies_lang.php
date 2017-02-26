d5b91b287c2f4eba7aeb8e6a9527111585bd23e9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:29:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'アカウントロックアウト';
+$lang['password_policies_app_description'] = 'パスワードポリシーのアプリは微調整のユーザパスワードポリシーのルールに使用されます。 これはあなたの基準を満たすパスワードを強制し、システムをより安全にするのに役立ちます。';
+$lang['password_policies_app_name'] = 'パスワードポリシー';
+$lang['password_policies_history_size'] = '履歴サイズ';
+$lang['password_policies_maximum_password_age'] = 'パスワードの有効期間';
+$lang['password_policies_minimum_password_age'] = 'パスワードの変更禁止期間';
+$lang['password_policies_minimum_password_length'] = '最小パスワード長';
+$lang['password_policies_modify_any_time'] = '任意の時間を変更します。';
+$lang['password_policies_no_expire'] = '無期限';
+$lang['password_policies_no_history'] = 'いいえ履歴はありません';
