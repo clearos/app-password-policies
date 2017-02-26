b9dbabef0efb081b283549da4376e57fa9681482
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:35:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'การล็อกบัญชี';
+$lang['password_policies_app_description'] = 'แอปพลิเคนโยบายรหัสผ่านที่ใช้ในการปรับแต่งกฎนโยบายรหัสผ่านผู้ใช้ นี้บังคับใช้รหัสผ่านที่ตรงกับเกณฑ์ของคุณและช่วยให้ระบบของคุณปลอดภัยมากขึ้น';
+$lang['password_policies_app_name'] = 'นโยบายรหัสผ่าน';
+$lang['password_policies_history_size'] = 'ขนาดประวัติศาสตร์';
+$lang['password_policies_maximum_password_age'] = 'สูงสุดอายุรหัสผ่าน';
+$lang['password_policies_minimum_password_age'] = 'รหัสผ่านขั้นต่ำอายุ';
+$lang['password_policies_minimum_password_length'] = 'ความยาวรหัสผ่านขั้นต่ำ';
+$lang['password_policies_modify_any_time'] = 'การปรับเปลี่ยนตลอดเวลา';
+$lang['password_policies_no_expire'] = 'ไม่มีหมดอายุ';
+$lang['password_policies_no_history'] = 'ไม่มีประวัติ';
