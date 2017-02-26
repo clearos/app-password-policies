035996b271a7abf8cb2e357d052a7c6f6d6f7858
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:34:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'ఖాతా నిర్బంధం';
+$lang['password_policies_app_description'] = 'పాస్వర్డ్ విధానం అనువర్తనాన్ని జరిమానా ట్యూన్ వినియోగదారు పాస్వర్డ్ విధానం నియమాలు ఉపయోగిస్తారు. ఈ మీ ప్రమాణానికి సరిపోయే పాస్వర్డ్లను అమలు చేస్తాడు మీ సిస్టమ్ భద్రతను మరింత సహాయపడుతుంది.';
+$lang['password_policies_app_name'] = 'పాస్వర్డ్ విధానాలు';
+$lang['password_policies_history_size'] = 'చరిత్ర సైజు';
+$lang['password_policies_maximum_password_age'] = 'గరిష్ఠ పాస్వర్డ్ వయసు';
+$lang['password_policies_minimum_password_age'] = 'కనీస పాస్వర్డ్ వయసు';
+$lang['password_policies_minimum_password_length'] = 'కనీస వర్డ్ పొడవు';
+$lang['password_policies_modify_any_time'] = 'ఏదైనా సమయం సవరించు';
+$lang['password_policies_no_expire'] = 'తోబుట్టువుల గడువు';
+$lang['password_policies_no_history'] = 'తోబుట్టువుల చరిత్ర';
