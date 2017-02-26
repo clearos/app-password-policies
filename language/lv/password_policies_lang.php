15fc093a5b5d7cb25ea11b82794fe57ed8671489
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:30:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'konta bloķēšana';
+$lang['password_policies_app_description'] = 'Password politika app izmanto, lai precīzi noskaņotu lietotāju paroles politikas noteikumiem. Tas īsteno paroles, kas atbilst jūsu kritērijiem un palīdz padarīt jūsu sistēmu drošāku.';
+$lang['password_policies_app_name'] = 'parole politika';
+$lang['password_policies_history_size'] = 'Vēsture Size';
+$lang['password_policies_maximum_password_age'] = 'Maksimālais Password Age';
+$lang['password_policies_minimum_password_age'] = 'Minimālā Password Age';
+$lang['password_policies_minimum_password_length'] = 'Minimālā Paroles garums';
+$lang['password_policies_modify_any_time'] = 'Mainīt jebkurā laikā';
+$lang['password_policies_no_expire'] = 'nav Aizvākt';
+$lang['password_policies_no_history'] = 'nav vēsture';
