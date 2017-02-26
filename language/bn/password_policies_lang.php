4603d191c17ebd72b15eeab4a09e7513bdaf4170
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:25:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'একাউন্ট লকআউট';
+$lang['password_policies_app_description'] = 'পাসওয়ার্ড নীতি অ্যাপ্লিকেশান জরিমানা ব্যবহারকারী সুর পাসওয়ার্ড নীতি নিয়ম করতে ব্যবহৃত হয়. এই পাসওয়ার্ড যে আপনার মানদণ্ড পূরণ enforces এবং আপনার সিস্টেমে আরো নিরাপদ করতে সহায়তা করে.';
+$lang['password_policies_app_name'] = 'পাসওয়ার্ড নীতিসমূহ';
+$lang['password_policies_history_size'] = 'ইতিহাসের আয়তন';
+$lang['password_policies_maximum_password_age'] = 'সর্বাধিক পাসওয়ার্ড বয়স';
+$lang['password_policies_minimum_password_age'] = 'সর্বনিম্ন পাসওয়ার্ড বয়স';
+$lang['password_policies_minimum_password_length'] = 'পাসওয়ার্ডের ন্যূনতম দৈর্ঘ্য';
+$lang['password_policies_modify_any_time'] = 'পরিবর্তন কোন সময়';
+$lang['password_policies_no_expire'] = 'কোন Expire';
+$lang['password_policies_no_history'] = 'কোন ইতিহাস';
