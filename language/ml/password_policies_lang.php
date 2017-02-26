cc99f4324a542630ebc3bf92b8fa4589c9ecaef7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:31:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'അക്കൗണ്ട് മാനേജര്';
+$lang['password_policies_app_description'] = 'പാസ്വേഡ് നയം അപ്ലിക്കേഷൻ നന്നായി ഉപയോക്തൃ പാസ്വേഡ് നയം നിയമങ്ങൾ ഉപയോഗിക്കുന്നു. ഇത് നിങ്ങളുടെ മാനദണ്ഡം പാലിക്കുന്ന പാസ്വേഡുകൾ ബാധകമാകുക നിങ്ങളുടെ സിസ്റ്റം കൂടുതൽ സുരക്ഷിതമാക്കും സഹായിക്കുന്നു.';
+$lang['password_policies_app_name'] = 'പാസ്വേഡ് നയങ്ങള്';
+$lang['password_policies_history_size'] = 'ചരിത്രം വലിപ്പം';
+$lang['password_policies_maximum_password_age'] = 'പരമാവധി പാസ്വേഡ് പ്രായം';
+$lang['password_policies_minimum_password_age'] = 'കുറഞ്ഞ പാസ്വേഡ് പ്രായം';
+$lang['password_policies_minimum_password_length'] = 'കുറഞ്ഞ പാസ്വേഡ് ദൈർഘ്യം';
+$lang['password_policies_modify_any_time'] = 'എപ്പോഴും പരിഷ്ക്കരിക്കുക';
+$lang['password_policies_no_expire'] = 'ഇല്ല തീരുന്ന';
+$lang['password_policies_no_history'] = 'ചരിത്രം ഇല്ല';
