fa89d08462653c1501e03af5caff50571eb601d6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:24:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'rekening Lockout';
+$lang['password_policies_app_description'] = 'Die wagwoord Beleid inligting word gebruik om fyn wagwoord beleid reëls deuntjie gebruiker. Dit dwing wagwoorde wat aan jou kriteria en help om jou stelsel meer veilig te maak.';
+$lang['password_policies_app_name'] = 'wagwoord beleid';
+$lang['password_policies_history_size'] = 'geskiedenis Grootte';
+$lang['password_policies_maximum_password_age'] = 'Maksimum wagwoord Ouderdom';
+$lang['password_policies_minimum_password_age'] = 'Minimum wagwoord Ouderdom';
+$lang['password_policies_minimum_password_length'] = 'Minimum wagwoord lengte';
+$lang['password_policies_modify_any_time'] = 'Verander Enige Tyd';
+$lang['password_policies_no_expire'] = 'geen verloop';
+$lang['password_policies_no_history'] = 'geen Geskiedenis';
