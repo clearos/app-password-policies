2becb1e7c3681e8cf59deb8cdea2598ce67d5341
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:30:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'propter Lockout';
+$lang['password_policies_app_description'] = 'Password Policy de app est, ut tune fine user Password consilium regulas. Id quo facilius facere tuetur Tesserae fefe elit systema securus.';
+$lang['password_policies_app_name'] = 'Password Politiae';
+$lang['password_policies_history_size'] = 'Historia Size';
+$lang['password_policies_maximum_password_age'] = 'Maximum Age Password';
+$lang['password_policies_minimum_password_age'] = 'Minimum: Password Age';
+$lang['password_policies_minimum_password_length'] = 'Minimum: Password Length';
+$lang['password_policies_modify_any_time'] = 'Temperare Omne Tempus';
+$lang['password_policies_no_expire'] = 'non Expire';
+$lang['password_policies_no_history'] = 'non historia';
