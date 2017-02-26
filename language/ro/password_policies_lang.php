c79d70d4b0af6fa99b7a44b5a58afde29a820e6b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:33:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'contul Lockout';
+$lang['password_policies_app_description'] = 'Aplicația Politica de parolă este folosită pentru reguli fine de politică parola de utilizator ton. Acest lucru impune parole care îndeplinesc criteriile dvs. și ajută pentru a face sistemul mai sigur.';
+$lang['password_policies_app_name'] = 'Politicile de parolă';
+$lang['password_policies_history_size'] = 'Istoricul Dimensiune';
+$lang['password_policies_maximum_password_age'] = 'Maximă parolă Vârstă';
+$lang['password_policies_minimum_password_age'] = 'Varsta minima parola';
+$lang['password_policies_minimum_password_length'] = 'Lungimea minimă a parolei';
+$lang['password_policies_modify_any_time'] = 'Modifica orice moment';
+$lang['password_policies_no_expire'] = 'nr Expirație';
+$lang['password_policies_no_history'] = 'nu există niciun istoric';
