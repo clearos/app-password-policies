5080a48dbdc0b5b3345723303fb84437d1c96200
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:34:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'கணக்கு முடக்கம்';
+$lang['password_policies_app_description'] = 'கடவுச்சொல் கொள்கை பயன்பாட்டை நன்றாக இசைக்கு பயனர் கடவுச்சொல்லை கொள்கை விதிகள் பயன்படுத்தப்படுகிறது. இது உங்கள் நிபந்தனைகளுக்கு என்று கடவுச்சொற்களை அமுல்படுத்தல், உங்கள் கணினியில் மிகவும் பாதுகாப்பான செய்ய உதவுகிறது.';
+$lang['password_policies_app_name'] = 'கடவுச்சொல் கொள்கைகள்';
+$lang['password_policies_history_size'] = 'வரலாறு அளவு';
+$lang['password_policies_maximum_password_age'] = 'அதிகபட்ச கடவுச்சொல் வயது';
+$lang['password_policies_minimum_password_age'] = 'குறைந்தபட்ச கடவுச்சொல் வயது';
+$lang['password_policies_minimum_password_length'] = 'குறைந்தபட்ச கடவுச்சொல்';
+$lang['password_policies_modify_any_time'] = 'எந்த நேரம் மாற்ற';
+$lang['password_policies_no_expire'] = 'இல்லை காலாவதி';
+$lang['password_policies_no_history'] = 'வரலாறு இல்லை';
