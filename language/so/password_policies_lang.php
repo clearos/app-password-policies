d0fe49592cbac4bd12b5afbf4683525c4f3942d5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:33:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'Account Shaqoxannibista';
+$lang['password_policies_app_description'] = 'app Policy Password waxaa loo isticmaalaa in xeerarka siyaasadda password habayn user wanaagsan. Tani hirgelinaayo passwords in la kulmo shuruudaha iyo waxay ka caawisaa in la sameeyo nidaamka dheeraad ah oo aamin ah.';
+$lang['password_policies_app_name'] = 'Siyaasadaha Password';
+$lang['password_policies_history_size'] = 'Size History';
+$lang['password_policies_maximum_password_age'] = 'Age Password ugu badan';
+$lang['password_policies_minimum_password_age'] = 'Da&#39;da ugu yar Password';
+$lang['password_policies_minimum_password_length'] = 'Length Password Ugu Yar';
+$lang['password_policies_modify_any_time'] = 'Badalayo Time kasta';
+$lang['password_policies_no_expire'] = 'No dhacayo';
+$lang['password_policies_no_history'] = 'No History';
