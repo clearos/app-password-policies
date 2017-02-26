2b365fa1d919444da46035b7529283921cdce105
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:29:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'ანგარიშის ჩაკეტვა';
+$lang['password_policies_app_description'] = 'პაროლის Policy აპი გამოიყენება ჯარიმის სრულყოფილი შესახებ დაგავიწყდათ პოლიტიკის წესები. ეს აძლიერებს პაროლები, რომ დააკმაყოფილებს თქვენს კრიტერიუმებს და ეხმარება, რათა თქვენი სისტემის კიდევ უფრო უსაფრთხო.';
+$lang['password_policies_app_name'] = 'პაროლი პოლიტიკა';
+$lang['password_policies_history_size'] = 'ისტორია ზომა';
+$lang['password_policies_maximum_password_age'] = 'მაქსიმალური პაროლი ასაკი';
+$lang['password_policies_minimum_password_age'] = 'მინიმალური პაროლი ასაკი';
+$lang['password_policies_minimum_password_length'] = 'მინიმალური დაგავიწყდათ სიგრძე';
+$lang['password_policies_modify_any_time'] = 'ცვლილებები ნებისმიერ დროს';
+$lang['password_policies_no_expire'] = 'არ ეწურება';
+$lang['password_policies_no_history'] = 'ისტორია არ არის';
