16ea20da2a8d202554a0b10a7db10c9d765b82cd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:30:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'sąskaita Lockout';
+$lang['password_policies_app_description'] = 'Slaptažodžio Policy "naudojamas sureguliuoti Vartotojo slaptažodis politikos taisyklių. Tai įtvirtina slaptažodžius, kurie atitinka jūsų kriterijų ir padeda padaryti jūsų sistema saugesnis.';
+$lang['password_policies_app_name'] = 'Slaptažodis politika';
+$lang['password_policies_history_size'] = 'istorija dydis';
+$lang['password_policies_maximum_password_age'] = 'Maksimalus Slaptažodžių Amžius';
+$lang['password_policies_minimum_password_age'] = 'Minimalus Slaptažodžių Amžius';
+$lang['password_policies_minimum_password_length'] = 'Minimalus Slaptažodžių ilgis';
+$lang['password_policies_modify_any_time'] = 'Keisti bet kuriuo metu';
+$lang['password_policies_no_expire'] = 'Nėra Expire';
+$lang['password_policies_no_history'] = 'Nėra istorija';
