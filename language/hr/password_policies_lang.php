3768fb8a9ec963d3f1713698c212a99b05481140
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:26:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hr/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'Lockout račun';
+$lang['password_policies_app_description'] = 'Lozinka politika aplikacija se koristi za fino podešavanje pravila korisničke lozinke politike. To nameće lozinke koje zadovoljavaju vaše kriterije i pomaže kako bi vaš sustav sigurniji.';
+$lang['password_policies_app_name'] = 'Pravila Lozinka';
+$lang['password_policies_history_size'] = 'Povijest Veličina';
+$lang['password_policies_maximum_password_age'] = 'Maksimalna Lozinka Godine';
+$lang['password_policies_minimum_password_age'] = 'Minimalna Lozinka Godine';
+$lang['password_policies_minimum_password_length'] = 'Minimalna duljina lozinke';
+$lang['password_policies_modify_any_time'] = 'Izmjena Bilo koje vrijeme';
+$lang['password_policies_no_expire'] = 'Ne Ističe';
+$lang['password_policies_no_history'] = 'Ne Povijest';
