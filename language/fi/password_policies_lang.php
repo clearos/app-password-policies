db7ed52e3e6dbc5d08376a6eb5d317d7a2798bec
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:27:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'Tilin lukitus';
+$lang['password_policies_app_description'] = 'Password Policy -sovellus käytetään hienosäätöön käyttäjän salasanan toimintaperiaatteiden mukaisesti. Tämä valvoo salasanoja, jotka täyttävät hakuehdot ja auttaa tekemään järjestelmän turvallisuuden.';
+$lang['password_policies_app_name'] = 'salasana käytännöt';
+$lang['password_policies_history_size'] = 'History Size';
+$lang['password_policies_maximum_password_age'] = 'Maximum Password Ikä';
+$lang['password_policies_minimum_password_age'] = 'Pienin Salasana ikä';
+$lang['password_policies_minimum_password_length'] = 'Salasanan vähimmäispituus';
+$lang['password_policies_modify_any_time'] = 'Muokkaa Any Time';
+$lang['password_policies_no_expire'] = 'ei Expire';
+$lang['password_policies_no_history'] = 'ei historia';
