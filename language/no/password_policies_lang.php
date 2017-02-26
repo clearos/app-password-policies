35a63584300f20aeb76c7b91639e7c23931f84b4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:32:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'Account Lockout';
+$lang['password_policies_app_description'] = 'Passordregler app brukes til å fininnstille brukerpassord handlingsregler. Dette håndhever passord som oppfyller kriteriene og bidrar til å gjøre systemet mer sikkert.';
+$lang['password_policies_app_name'] = 'Passord Politikk';
+$lang['password_policies_history_size'] = 'historie Størrelse';
+$lang['password_policies_maximum_password_age'] = 'Maksimal passordalder';
+$lang['password_policies_minimum_password_age'] = 'Minimum passordalder';
+$lang['password_policies_minimum_password_length'] = 'Minimum Password Length';
+$lang['password_policies_modify_any_time'] = 'Endre Any Time';
+$lang['password_policies_no_expire'] = 'Ingen Utløp';
+$lang['password_policies_no_history'] = 'Ingen History';
