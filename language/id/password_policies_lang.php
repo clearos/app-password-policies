f04c00dafcca9522cc98aded9126cc7ec43a5518
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:28:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'akun Lockout';
+$lang['password_policies_app_description'] = 'Aplikasi Kebijakan Password digunakan untuk aturan kebijakan password selaras pengguna baik. Hal ini memaksa password yang memenuhi kriteria Anda dan membantu untuk membuat sistem anda lebih aman.';
+$lang['password_policies_app_name'] = 'Kebijakan sandi';
+$lang['password_policies_history_size'] = 'Ukuran sejarah';
+$lang['password_policies_maximum_password_age'] = 'Maksimum Sandi Umur';
+$lang['password_policies_minimum_password_age'] = 'Sandi Usia Minimum';
+$lang['password_policies_minimum_password_length'] = 'Sandi Panjang Minimum';
+$lang['password_policies_modify_any_time'] = 'Memodifikasi Setiap Waktu';
+$lang['password_policies_no_expire'] = 'Tidak Kadaluarsa';
+$lang['password_policies_no_history'] = 'Tidak ada Sejarah';
