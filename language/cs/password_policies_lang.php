92db12f2909f19d03708a72918fb78d930c43940
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:26:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'uzamčení účtu';
+$lang['password_policies_app_description'] = 'Heslo Policy aplikace slouží ke pravidel politiky hesla jemných uživatelských naladit. To vynutí hesla, které odpovídají zadaným kritériím a pomáhá, aby váš systém bezpečnější.';
+$lang['password_policies_app_name'] = 'hesel';
+$lang['password_policies_history_size'] = 'Historie Velikost';
+$lang['password_policies_maximum_password_age'] = 'Maximální stáří hesla';
+$lang['password_policies_minimum_password_age'] = 'Minimální stáří hesla';
+$lang['password_policies_minimum_password_length'] = 'Minimální délka hesla';
+$lang['password_policies_modify_any_time'] = 'Upravit Kdykoli';
+$lang['password_policies_no_expire'] = 'No vyprší';
+$lang['password_policies_no_history'] = 'No History';
