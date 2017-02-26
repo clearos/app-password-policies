9c5b1c38bd4fc0d2ca650283eb68d2af1012eccf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:26:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'nkhani Lockout';
+$lang['password_policies_app_description'] = 'The app achinsinsi Policy ntchito nyimbo wosuta malamulo abwino policy achinsinsi. Izi enforces mapasiwedi kuti amakwaniritsa anu amathandiza kuti dongosolo wanu otetezeka.';
+$lang['password_policies_app_name'] = 'Ndondomeko achinsinsi';
+$lang['password_policies_history_size'] = 'History Kukula';
+$lang['password_policies_maximum_password_age'] = 'Pazipita achinsinsi Age';
+$lang['password_policies_minimum_password_age'] = 'Osachepera achinsinsi Age';
+$lang['password_policies_minimum_password_length'] = 'Osachepera achinsinsi Utali';
+$lang['password_policies_modify_any_time'] = 'Kusintha nthawi iliyonse';
+$lang['password_policies_no_expire'] = 'No Kufa';
+$lang['password_policies_no_history'] = 'No History';
