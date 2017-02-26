199a5e5df5942ea7277b15d115ca1a12298cebfb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:31:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'заклучување на корисничката сметка';
+$lang['password_policies_app_description'] = 'Апликацијата за лозинка политика се користи за фино правила лозинка политика мелодија корисникот. Ова наметнува лозинки кои ги исполнуваат вашите критериуми и им помага да се направи вашиот систем посигурен.';
+$lang['password_policies_app_name'] = 'политики лозинка';
+$lang['password_policies_history_size'] = 'историја Големина';
+$lang['password_policies_maximum_password_age'] = 'Максимална Лозинка Години';
+$lang['password_policies_minimum_password_age'] = 'Минимална Лозинка Години';
+$lang['password_policies_minimum_password_length'] = 'Минимална Должина на лозинка';
+$lang['password_policies_modify_any_time'] = 'Измени во секое време';
+$lang['password_policies_no_expire'] = 'Не истекува';
+$lang['password_policies_no_history'] = 'Не Историја';
