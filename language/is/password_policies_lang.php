2fcd1ac561905862255e97865434dc554021be93
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:29:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'Reikningur Læsing';
+$lang['password_policies_app_description'] = 'The Password Policy app er notað til að fínstilla notendastefnu lykilorð reglum. Þetta knýja lykilorð sem uppfylla skilyrði þín og hjálpar til við að gera kerfið þitt öruggari.';
+$lang['password_policies_app_name'] = 'Lykilorð Reglur';
+$lang['password_policies_history_size'] = 'Saga Size';
+$lang['password_policies_maximum_password_age'] = 'Hámarks Lykilorð Age';
+$lang['password_policies_minimum_password_age'] = 'Lágmark Lykilorð Age';
+$lang['password_policies_minimum_password_length'] = 'Lágmark Lykilorð Lengd';
+$lang['password_policies_modify_any_time'] = 'Breyta hvenær';
+$lang['password_policies_no_expire'] = 'Nei renna út';
+$lang['password_policies_no_history'] = 'engin saga';
