104dee67e9c03d5a7fc428c5c3ac1ee89f9e01c5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:31:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'akaun Lockout';
+$lang['password_policies_app_description'] = 'Apl Dasar Kata laluan digunakan untuk kaedah-kaedah dasar kata laluan denda tune pengguna. Ini memperkuatkan kata laluan yang memenuhi kriteria anda dan membantu untuk membuat sistem anda lebih selamat.';
+$lang['password_policies_app_name'] = 'Polisi kata laluan';
+$lang['password_policies_history_size'] = 'Saiz Sejarah';
+$lang['password_policies_maximum_password_age'] = 'Kata laluan Umur Maksimum';
+$lang['password_policies_minimum_password_age'] = 'Kata laluan Umur Minimum';
+$lang['password_policies_minimum_password_length'] = 'Minimum Kata laluan Panjang';
+$lang['password_policies_modify_any_time'] = 'Ubahsuai Bila-bila Masa';
+$lang['password_policies_no_expire'] = 'Tiada Luputkan';
+$lang['password_policies_no_history'] = 'Tiada Sejarah';
