2570f4aafa719a8e200c7d97c5ac14a692d20f34
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:25:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'အကောင့် Lockout';
+$lang['password_policies_app_description'] = 'စကားဝှက်ပေါ်လစီ app ကိုဒဏ်ငွေတေးသွားကိုအသုံးပြုသူ password ကိုမူဝါဒကိုစည်းမျဉ်းစည်းကမ်းတွေကိုအသုံးပြုသည်။ ဒီနည်းနဲ့သင့်ရဲ့သတ်မှတ်ချက်ပြည့်မီကြောင်း passwords များကိုအတည်ပြုပေးမှာနှင့်သင့် system ကိုပိုမိုလုံခြုံစေရန်ကူညီပေးသည်။';
+$lang['password_policies_app_name'] = 'Password ကိုမူဝါဒများ';
+$lang['password_policies_history_size'] = 'သမိုင်း Size ကို';
+$lang['password_policies_maximum_password_age'] = 'အများဆုံး Password ကိုခေတ်';
+$lang['password_policies_minimum_password_age'] = 'နိမ့်ဆုံး Password ကိုခေတ်';
+$lang['password_policies_minimum_password_length'] = 'နိမ့်ဆုံး Password ကိုရှည်';
+$lang['password_policies_modify_any_time'] = 'မဆိုအချိန်ကိုပြုပြင်မွမ်းမံ';
+$lang['password_policies_no_expire'] = 'အဘယ်သူမျှမ Expire';
+$lang['password_policies_no_history'] = 'အဘယ်သူမျှမသမိုင်း';
