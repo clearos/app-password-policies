000e1f951667678d9d53b9172a5a8fc691b004be
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:34:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'kontoutelåsning';
+$lang['password_policies_app_description'] = 'Lösenord Policy-appen används för att finjustera användarlösenord policyregler. Detta framtvingar lösenord som uppfyller dina kriterier och bidrar till att göra systemet säkrare.';
+$lang['password_policies_app_name'] = 'lösenordsprinciper';
+$lang['password_policies_history_size'] = 'historia Storlek';
+$lang['password_policies_maximum_password_age'] = 'Maximal Password Ålder';
+$lang['password_policies_minimum_password_age'] = 'Minsta Password Ålder';
+$lang['password_policies_minimum_password_length'] = 'Minsta längd på lösenord';
+$lang['password_policies_modify_any_time'] = 'Ändra Any Time';
+$lang['password_policies_no_expire'] = 'ingen Löper';
+$lang['password_policies_no_history'] = 'ingen historia';
