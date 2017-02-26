839d954940806312534eca5e483f0bd2f1d8d221
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:28:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'fiókzárolási';
+$lang['password_policies_app_description'] = 'A jelszó alkalmazást használnak finomhangolása felhasználó jelszó politika szabályait. Ez kikényszeríti a jelszavakat, amelyek megfelelnek a feltételeknek, és segít abban, hogy a rendszer biztonságosabbá tétele érdekében.';
+$lang['password_policies_app_name'] = 'jelszóirányelvek';
+$lang['password_policies_history_size'] = 'History Méret';
+$lang['password_policies_maximum_password_age'] = 'Maximális jelszó Age';
+$lang['password_policies_minimum_password_age'] = 'Jelszó minimális korát';
+$lang['password_policies_minimum_password_length'] = 'A jelszó minimális hossza';
+$lang['password_policies_modify_any_time'] = 'Bármikor megváltoztat-';
+$lang['password_policies_no_expire'] = 'nem Lejár';
+$lang['password_policies_no_history'] = 'nem előzmények';
