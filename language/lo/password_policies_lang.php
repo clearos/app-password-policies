69165735e5e6c57a83dcbce33dceb1510c1c6f9e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:30:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'Lockout ບັນຊີ';
+$lang['password_policies_app_description'] = 'ການ app ນະໂຍບາຍລະຫັດຜ່ານໄດ້ຖືກນໍາໃຊ້ກັບຜູ້ໃຊ້ປບັລະບຽບການນະໂຍບາຍລະຫັດຜ່ານອັນດີງາມ. ນີ້ບັງຄັບໃຊ້ລະຫັດຜ່ານທີ່ຕອບສະຫນອງເງື່ອນໄຂຂອງທ່ານແລະຈະຊ່ວຍໃຫ້ການເຮັດໃຫ້ລະບົບຂອງທ່ານຮັບປະກັນກວ່າ.';
+$lang['password_policies_app_name'] = 'ນະໂຍບາຍລະຫັດຜ່ານ';
+$lang['password_policies_history_size'] = 'ຂະຫນາດປະຫວັດສາດ';
+$lang['password_policies_maximum_password_age'] = 'ສູງສຸດລະຫັດຜ່ານອາຍຸ';
+$lang['password_policies_minimum_password_age'] = 'ຂັ້ນຕ່ໍາລະຫັດຜ່ານອາຍຸ';
+$lang['password_policies_minimum_password_length'] = 'Length ລະຫັດຜ່ານຕໍາ່ສຸດທີ່';
+$lang['password_policies_modify_any_time'] = 'ປັບປຸງແກ້ໄຂເວລາໃດ';
+$lang['password_policies_no_expire'] = 'ບໍ່ມີຫມົດອາຍຸ';
+$lang['password_policies_no_history'] = 'ບໍ່ມີປະຫວັດການ';
