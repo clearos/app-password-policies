a5ea7f6d68bb0e3f3f0023bcc74f7104c334b126
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:24:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'Llogaria lokaut';
+$lang['password_policies_app_description'] = 'App Password Politika është përdorur për të rregullave të bukura të politikave fjalëkalimin e përdoruesit të akorduar. Kjo zbaton fjalëkalimet që përmbushin kriteret tuaja dhe ndihmon për ta bërë sistemin tuaj më të sigurt.';
+$lang['password_policies_app_name'] = 'Politikat Fjalëkalimi';
+$lang['password_policies_history_size'] = 'Historia Size';
+$lang['password_policies_maximum_password_age'] = 'Maximum Password Age';
+$lang['password_policies_minimum_password_age'] = 'Minimum Password Age';
+$lang['password_policies_minimum_password_length'] = 'Minimum Password Length';
+$lang['password_policies_modify_any_time'] = 'Modifiko çdo kohë';
+$lang['password_policies_no_expire'] = 'No Skadimi';
+$lang['password_policies_no_history'] = 'No History';
