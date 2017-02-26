69887cbf88a6a4ece29ca19fedc35a7b1beafdce
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:34:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'Account Lockout';
+$lang['password_policies_app_description'] = 'The app e Password Policy e sebelisoa ho Tune user want-meet.ru melawana e metle leano la phasewete. Sena se enforces diphasewete hore kopana dihlahlobo tse ya hao le ho thusa ho etsa ya hao tsamaiso sireletsehile.';
+$lang['password_policies_app_name'] = 'Maano Password';
+$lang['password_policies_history_size'] = 'History size';
+$lang['password_policies_maximum_password_age'] = 'Palo e kahodimodimo Password Age';
+$lang['password_policies_minimum_password_age'] = 'Bonyane ba Password Age';
+$lang['password_policies_minimum_password_length'] = 'Bonyane ba Password Length';
+$lang['password_policies_modify_any_time'] = 'Fetoloa nako efe kapa efe';
+$lang['password_policies_no_expire'] = 'No shoa';
+$lang['password_policies_no_history'] = 'No History';
