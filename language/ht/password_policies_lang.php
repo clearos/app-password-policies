148462b5a85e5755c27e26058e25e2974a44644f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:27:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'Kont verouyaj';
+$lang['password_policies_app_description'] = 'Se app a Policy Modpas itilize sou ton itilizatè règ amann politik modpas. Sa a aplike modpas ki satisfè kritè ou yo epi li ede fè sistèm ou plis sekirite.';
+$lang['password_policies_app_name'] = 'Règleman Modpas';
+$lang['password_policies_history_size'] = 'Istwa Kantite moun ki';
+$lang['password_policies_maximum_password_age'] = 'Maksimòm Modpas Laj';
+$lang['password_policies_minimum_password_age'] = 'Minimòm Modpas Laj';
+$lang['password_policies_minimum_password_length'] = 'Minimòm Length Modpas';
+$lang['password_policies_modify_any_time'] = 'Modifye Nenpòt Lè';
+$lang['password_policies_no_expire'] = 'Pa gen Ekspire';
+$lang['password_policies_no_history'] = 'Pa gen Istwa';
