62d254db9c5493db34ea48b6e0dd0854f87a87a5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:26:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'bloqueig de compte';
+$lang['password_policies_app_description'] = 'L&#39;aplicació de política d&#39;contrasenya s&#39;utilitza per regles fines directiva de contrasenyes d&#39;usuari sintonitzar. Això fa complir les contrasenyes que compleixen els seus criteris i ajuda a fer que el seu sistema sigui més segur.';
+$lang['password_policies_app_name'] = 'Directives de contrasenya';
+$lang['password_policies_history_size'] = 'història Mida';
+$lang['password_policies_maximum_password_age'] = 'Màxima de la contrasenya';
+$lang['password_policies_minimum_password_age'] = 'Mínima de la contrasenya';
+$lang['password_policies_minimum_password_length'] = 'Longitud mínima de la contrasenya';
+$lang['password_policies_modify_any_time'] = 'modificar Jornada';
+$lang['password_policies_no_expire'] = 'sense caducar';
+$lang['password_policies_no_history'] = 'no històric';
