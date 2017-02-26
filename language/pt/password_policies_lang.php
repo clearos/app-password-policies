90fed3e3f696a5a7787b7eda0db287b2e48f6545
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:32:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pt/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'Bloqueio de conta';
+$lang['password_policies_app_description'] = 'O aplicativo da Política de senha é usado para regras de política de senha do usuário Ajuste fino. Isso reforça as senhas que atendem aos seus critérios e ajuda a tornar o sistema mais seguro.';
+$lang['password_policies_app_name'] = 'Diretivas de senha';
+$lang['password_policies_history_size'] = 'Histórico Tamanho';
+$lang['password_policies_maximum_password_age'] = 'Tempo máximo da senha';
+$lang['password_policies_minimum_password_age'] = 'Minimum Password Age';
+$lang['password_policies_minimum_password_length'] = 'Senha comprimento mínimo';
+$lang['password_policies_modify_any_time'] = 'Modificar Qualquer Hora';
+$lang['password_policies_no_expire'] = 'Sem Expire';
+$lang['password_policies_no_history'] = 'No History';
