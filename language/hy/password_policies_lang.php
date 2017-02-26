e0cfacc6dc4c1deab690a2c4c522645a68eda8d9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:24:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'Հաշվի լոկաուտ';
+$lang['password_policies_app_description'] = 'The Գաղտնաբառ քաղաքականությունը ծրագիրը, որն օգտագործվում է կերպարվեստի մեղեդի անձնագիրը գաղտնաբառը քաղաքականության կանոնների: Սա հարկադրում գաղտնաբառերը որոնք համապատասխանում են Ձեր չափանիշներին եւ օգնում է, որպեսզի ձեր համակարգը ավելի ապահով.';
+$lang['password_policies_app_name'] = 'Գաղտնաբառ քաղաքականություն';
+$lang['password_policies_history_size'] = 'պատմություն Size';
+$lang['password_policies_maximum_password_age'] = 'Առավելագույն Գաղտնաբառ Տարիք';
+$lang['password_policies_minimum_password_age'] = 'Նվազագույն Գաղտնաբառ Տարիք';
+$lang['password_policies_minimum_password_length'] = 'Նվազագույն Գաղտնաբառ Length';
+$lang['password_policies_modify_any_time'] = 'Փոփոխել ցանկացած ժամանակ';
+$lang['password_policies_no_expire'] = 'ոչ ավարտվում';
+$lang['password_policies_no_history'] = 'ոչ պատմություն';
