6efce0fd3f626f931532c43d7a6d714ad93c126a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:33:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'блокировки учетной записи';
+$lang['password_policies_app_description'] = 'Приложение Политика паролей используется для тонких правил политики паролей пользователей настроиться. Это обеспечивает соблюдение пароли, которые отвечают вашим критериям и помогает сделать вашу систему более безопасной.';
+$lang['password_policies_app_name'] = 'политики паролей';
+$lang['password_policies_history_size'] = 'История Размер';
+$lang['password_policies_maximum_password_age'] = 'Максимальный срок действия пароля';
+$lang['password_policies_minimum_password_age'] = 'Минимальный срок действия пароля';
+$lang['password_policies_minimum_password_length'] = 'Минимальная длина пароля';
+$lang['password_policies_modify_any_time'] = 'Изменить любое время';
+$lang['password_policies_no_expire'] = 'Нет Expire';
+$lang['password_policies_no_history'] = 'Нет истории';
