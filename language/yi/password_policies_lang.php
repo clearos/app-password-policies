8bc305c70f11587352d43426145b9f0e0ca1e855
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:36:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'אַקאַונט לאַקאַוט';
+$lang['password_policies_app_description'] = 'די פּאַראָל פּאָליטיק אַפּ איז געניצט צו פייַן ניגן באַניצער פּאַראָל פּאָליטיק כּללים. דאס ענפאָרסעס פּאַסווערדז אַז טרעפן דיין קרייטיריאַ און העלפּס צו מאַכן אייער סיסטעם מער זיכער.';
+$lang['password_policies_app_name'] = 'שפּריכוואָרט פּאָליסיעס';
+$lang['password_policies_history_size'] = 'געשיכטע גרייס';
+$lang['password_policies_maximum_password_age'] = 'מאַקסימום פּאַראָל צייט';
+$lang['password_policies_minimum_password_age'] = 'מינימום פּאַראָל צייט';
+$lang['password_policies_minimum_password_length'] = 'מינימום פּאַראָל לענג';
+$lang['password_policies_modify_any_time'] = 'מאָדיפיצירן קיין מאָל';
+$lang['password_policies_no_expire'] = 'ניט קיין עקספּירע';
+$lang['password_policies_no_history'] = 'קיין געשיכטע';
