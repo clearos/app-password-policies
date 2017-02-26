10b2d9b9fa076290bd6e65a6443049e2eac188af
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:34:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'akun Lockout';
+$lang['password_policies_app_description'] = 'Aplikasi Kawijakan Password dipaké pikeun aturan kawijakan password Ngepaskeun pamaké rupa. Ieu enforces kecap akses nu minuhan kriteria Anjeun tur mantuan nyieun sistim Anjeun leuwih aman.';
+$lang['password_policies_app_name'] = 'Kawijakan sandi';
+$lang['password_policies_history_size'] = 'Ukuran Sajarah';
+$lang['password_policies_maximum_password_age'] = 'Maximum Password Jaman';
+$lang['password_policies_minimum_password_age'] = 'Password Jaman minimum';
+$lang['password_policies_minimum_password_length'] = 'Password Panjang minimum';
+$lang['password_policies_modify_any_time'] = 'Ngarobah Sakur Time';
+$lang['password_policies_no_expire'] = 'no tamat';
+$lang['password_policies_no_history'] = 'no Sajarah';
