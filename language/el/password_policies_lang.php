b641dfc54f7ebb23570d61b6814547899aa04a5d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:27:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'Ο Λογαριασμός Lockout';
+$lang['password_policies_app_description'] = 'Η εφαρμογή της πολιτικής κωδικού πρόσβασης χρησιμοποιείται για τους κανόνες της πολιτικής κωδικό πρόστιμο χρήστη μελωδία. Αυτό επιβάλλει κωδικούς πρόσβασης που πληρούν τα κριτήρια σας και σας βοηθά να κάνετε το σύστημά σας πιο ασφαλές.';
+$lang['password_policies_app_name'] = 'Πολιτικές κωδικού πρόσβασης';
+$lang['password_policies_history_size'] = 'Ιστορία Μέγεθος';
+$lang['password_policies_maximum_password_age'] = 'Μέγιστη ηλικία κωδικού πρόσβασης';
+$lang['password_policies_minimum_password_age'] = 'Κατώτατο όριο ηλικίας κωδικό πρόσβασης';
+$lang['password_policies_minimum_password_length'] = 'Ελάχιστο μήκος του κωδικού πρόσβασης';
+$lang['password_policies_modify_any_time'] = 'Τροποποίηση Any Time';
+$lang['password_policies_no_expire'] = 'δεν Λήγει';
+$lang['password_policies_no_history'] = 'δεν Ιστορία';
