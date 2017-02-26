59573a3f8142fc5697220eae61c19757a41d8935
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:27:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fr/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'verrouillage de compte';
+$lang['password_policies_app_description'] = 'L&#39;application de la politique de mot de passe est utilisé pour fins utilisateur tune règles de politique de mot de passe. Ceci impose des mots de passe qui correspondent à vos critères et aide à rendre votre système plus sûr.';
+$lang['password_policies_app_name'] = 'Mot de passe Politiques';
+$lang['password_policies_history_size'] = 'Histoire Taille';
+$lang['password_policies_maximum_password_age'] = 'Maximum Password Age';
+$lang['password_policies_minimum_password_age'] = 'Minimum Password Age';
+$lang['password_policies_minimum_password_length'] = 'Minimum Password Length';
+$lang['password_policies_modify_any_time'] = 'Modifier Any Time';
+$lang['password_policies_no_expire'] = 'No Expiration';
+$lang['password_policies_no_history'] = 'Non Histoire';
