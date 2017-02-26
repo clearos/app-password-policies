8eb1822b3b0ce2fee5657b90e0f3d267fdb6f451
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:34:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/es/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'bloqueo de cuenta';
+$lang['password_policies_app_description'] = 'La aplicación de política de contraseña se utiliza para reglas finas directiva de contraseñas de usuario sintonizar. Esto hace cumplir las contraseñas que cumplen sus criterios y ayuda a hacer que su sistema sea más seguro.';
+$lang['password_policies_app_name'] = 'Directivas de contraseña';
+$lang['password_policies_history_size'] = 'historia Tamaño';
+$lang['password_policies_maximum_password_age'] = 'Máxima de la contraseña';
+$lang['password_policies_minimum_password_age'] = 'Mínima de la contraseña';
+$lang['password_policies_minimum_password_length'] = 'Longitud mínima de contraseña';
+$lang['password_policies_modify_any_time'] = 'Modificar Jornada';
+$lang['password_policies_no_expire'] = 'sin caducar';
+$lang['password_policies_no_history'] = 'No Historial';
