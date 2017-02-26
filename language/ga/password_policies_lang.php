bcfdec96c783a10a8cc133b15f9fb41b0a9265c7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:28:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'Cuntas Lockout';
+$lang['password_policies_app_description'] = 'Is é an app Polasaí Pasfhocal úsáidtear chun rialacha maidir le beartas focal faire don úsáideoir tune fíneáil. enforces sé seo focal faire a chomhlíonann do chritéir agus cuidíonn a dhéanamh do chóras níos sláine.';
+$lang['password_policies_app_name'] = 'Polasaithe Pasfhocal';
+$lang['password_policies_history_size'] = 'Stair Méid';
+$lang['password_policies_maximum_password_age'] = 'Uasmhéid Pasfhocal Aois';
+$lang['password_policies_minimum_password_age'] = 'Íosta Pasfhocal Aois';
+$lang['password_policies_minimum_password_length'] = 'Íosta Suntasacha Pasfhocal';
+$lang['password_policies_modify_any_time'] = 'Athraigh Aon Am';
+$lang['password_policies_no_expire'] = 'Níl éag';
+$lang['password_policies_no_history'] = 'Gan Stair';
