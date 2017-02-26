965294655adb4de01052391355354a4d460aa79b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:25:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bg/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'Account Lockout';
+$lang['password_policies_app_description'] = 'Приложението политика парола се използва за фини правила на политиката парола мелодия на потребителите. Това налага пароли, които отговарят на следните критерии и помага да се направи системата ви по-сигурен.';
+$lang['password_policies_app_name'] = 'политики за парола';
+$lang['password_policies_history_size'] = 'История Размер';
+$lang['password_policies_maximum_password_age'] = 'Максимална Парола Възраст';
+$lang['password_policies_minimum_password_age'] = 'Минимална Парола Възраст';
+$lang['password_policies_minimum_password_length'] = 'Минимална Парола Дължина';
+$lang['password_policies_modify_any_time'] = 'Промяна Всяко Време';
+$lang['password_policies_no_expire'] = 'Не Изтичане';
+$lang['password_policies_no_history'] = 'Няма история';
