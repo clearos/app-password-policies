c16f3379709719052d6b9beb73e96e49905fb521
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:29:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'тіркелгі құрсаулау';
+$lang['password_policies_app_description'] = 'Құпия сөз саясаты Қолданба баптаңыз пайдаланушы құпия саясаты ережелері қолданылады. Бұл сіздің өлшемдерге сай құпия сөздерді қолданады және Сіздің жүйесі неғұрлым қауіпсіз жасауға көмектеседі.';
+$lang['password_policies_app_name'] = 'Құпия сөз саясаты';
+$lang['password_policies_history_size'] = 'Тарих өлшемі';
+$lang['password_policies_maximum_password_age'] = 'Ең Password Жасы';
+$lang['password_policies_minimum_password_age'] = 'Ең төменгі Password Жасы';
+$lang['password_policies_minimum_password_length'] = 'Ең төменгі Пароль ұзындығы';
+$lang['password_policies_modify_any_time'] = 'Кез келген уақытта өзгерту';
+$lang['password_policies_no_expire'] = 'жоқ Ескілердентазалау';
+$lang['password_policies_no_history'] = 'тарихы жоқ';
