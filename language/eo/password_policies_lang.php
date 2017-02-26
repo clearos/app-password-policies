b09b583899cd2c02d3b082460825a30e7a5d3759
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:26:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'konto Lockout';
+$lang['password_policies_app_description'] = 'La pasvorto Politiko app kutimas fajna melodio pasvorton politiko reguloj. Ĉi efikigas pasvortojn kiuj renkonti viajn kriterioj kaj helpas fari vian sistemon pli sekura.';
+$lang['password_policies_app_name'] = 'Pasvorto Politikoj';
+$lang['password_policies_history_size'] = 'historio Grandeco';
+$lang['password_policies_maximum_password_age'] = 'Maksimuma Pasvorto Aĝo';
+$lang['password_policies_minimum_password_age'] = 'Minimuma Pasvorto Aĝo';
+$lang['password_policies_minimum_password_length'] = 'Minimuma Pasvorto Longeco';
+$lang['password_policies_modify_any_time'] = 'Modifi Ajna Tempo';
+$lang['password_policies_no_expire'] = 'neniu Forigi';
+$lang['password_policies_no_history'] = 'neniu Historio';
