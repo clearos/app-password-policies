9729d14df411d5f29326e9c579cb82dfa9d53e0a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:35:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'account Lockout';
+$lang['password_policies_app_description'] = 'Inilalarawan ng Patakaran sa Password app ay ginagamit upang fine patakaran patakaran password tune user. Ito enforces password na tumutugma sa iyong pamantayan at tumutulong upang gumawa ng iyong system na mas secure.';
+$lang['password_policies_app_name'] = 'Mga Patakaran password';
+$lang['password_policies_history_size'] = 'Size history';
+$lang['password_policies_maximum_password_age'] = 'Maximum Password Edad';
+$lang['password_policies_minimum_password_age'] = 'Minimum Password Edad';
+$lang['password_policies_minimum_password_length'] = 'Minimum na Haba Password';
+$lang['password_policies_modify_any_time'] = 'Baguhin Anumang Oras';
+$lang['password_policies_no_expire'] = 'walang e-expire';
+$lang['password_policies_no_history'] = 'No History';
