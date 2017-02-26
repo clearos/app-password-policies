3f6fdfa9f8f2424957b195a5ed0d0e4d3aad8a66
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:30:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = '계정 잠금';
+$lang['password_policies_app_description'] = '암호 정책 앱은 미세 조정 사용자 암호 정책 규칙을 사용합니다. 이것은 당신의 기준을 충족하는 암호를 적용하고 시스템을보다 안전하게하는 데 도움이됩니다.';
+$lang['password_policies_app_name'] = '암호 정책';
+$lang['password_policies_history_size'] = '기록 크기';
+$lang['password_policies_maximum_password_age'] = '최대 암호 사용 기간';
+$lang['password_policies_minimum_password_age'] = '최소 암호 사용 기간';
+$lang['password_policies_minimum_password_length'] = '최소 암호 길이';
+$lang['password_policies_modify_any_time'] = '모든 시간을 수정';
+$lang['password_policies_no_expire'] = '아니 만료되지';
+$lang['password_policies_no_history'] = '어떤 기록하지';
