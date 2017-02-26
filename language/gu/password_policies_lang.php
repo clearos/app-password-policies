2e98d160f0d77615118c5c0f0972302f66b1b1e7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:27:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'ખાતું લૉકઆઉટ';
+$lang['password_policies_app_description'] = 'પાસવર્ડ નીતિ એપ્લિકેશન દંડ વપરાશકર્તા ટ્યુન પાસવર્ડ નીતિ નિયમો માટે વપરાય છે. આ પાસવર્ડો તમારા માપદંડ લાગુ અને તમારી સિસ્ટમ વધુ સુરક્ષિત બનાવવા માટે મદદ કરે છે.';
+$lang['password_policies_app_name'] = 'પાસવર્ડ નીતિઓ';
+$lang['password_policies_history_size'] = 'ઇતિહાસ માપ';
+$lang['password_policies_maximum_password_age'] = 'મહત્તમ પાસવર્ડ ઉંમર';
+$lang['password_policies_minimum_password_age'] = 'ન્યૂનતમ પાસવર્ડ ઉંમર';
+$lang['password_policies_minimum_password_length'] = 'ન્યૂનતમ પાસવર્ડ લંબાઈ';
+$lang['password_policies_modify_any_time'] = 'સુધારો કોઈપણ સમયનો';
+$lang['password_policies_no_expire'] = 'કોઈ સમયમર્યાદા સમાપ્તિ';
+$lang['password_policies_no_history'] = 'કોઈ ઇતિહાસ';
