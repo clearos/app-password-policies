dfed9758d733cb8c2539a7e021e44f9145f085c2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:28:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'account lockout';
+$lang['password_policies_app_description'] = 'The Password Policy app da ake amfani da m tune mai amfani da kalmar sirri siyasa dokoki. Wannan enforces kalmomin shiga da saduwa your sharudda da kuma taimaka don yin your tsarin more amintattu.';
+$lang['password_policies_app_name'] = 'Password Asiri';
+$lang['password_policies_history_size'] = 'History Size';
+$lang['password_policies_maximum_password_age'] = 'Maximum Password Age';
+$lang['password_policies_minimum_password_age'] = 'Mafi qarancin Password Age';
+$lang['password_policies_minimum_password_length'] = 'Mafi qarancin Password Length';
+$lang['password_policies_modify_any_time'] = 'Gyara Duk wani Time';
+$lang['password_policies_no_expire'] = 'No mutu';
+$lang['password_policies_no_history'] = 'No History';
