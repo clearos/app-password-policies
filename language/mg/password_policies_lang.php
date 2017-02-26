15870c061f5bfcf049fbe0dc505b43445d1f8f16
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:30:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'Account Lockout';
+$lang['password_policies_app_description'] = 'Ny Password Policy fampiharana no ampiasaina amin&#39;ny tsara miray feo mpampiasa tenimiafina fitsipika politika. Izany manamafy tenimiafina izay hihaona ny fepetra ary manampy mba hahatonga ny rafitra azo antoka kokoa.';
+$lang['password_policies_app_name'] = 'Password Policies';
+$lang['password_policies_history_size'] = 'History Size';
+$lang['password_policies_maximum_password_age'] = 'Maximum Password Age';
+$lang['password_policies_minimum_password_age'] = 'Minimum Password Age';
+$lang['password_policies_minimum_password_length'] = 'Minimum Password Length';
+$lang['password_policies_modify_any_time'] = 'Manova Any Time';
+$lang['password_policies_no_expire'] = 'No Expire';
+$lang['password_policies_no_history'] = 'No History';
