4621e3210aef2a86a109e798ff0f1bb8a1319ee4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:26:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = '帐户锁定';
+$lang['password_policies_app_description'] = '密码策略的应用是用来微调用户密码策略规则。 这加强了符合条件的密码，并有助于使您的系统更安全。';
+$lang['password_policies_app_name'] = '密码策略';
+$lang['password_policies_history_size'] = '历史记录大小';
+$lang['password_policies_maximum_password_age'] = '密码最长使用期限';
+$lang['password_policies_minimum_password_age'] = '密码最短使用期限';
+$lang['password_policies_minimum_password_length'] = '最小密码长度';
+$lang['password_policies_modify_any_time'] = '修改任何时间';
+$lang['password_policies_no_expire'] = '没有过期';
+$lang['password_policies_no_history'] = '没有历史记录';
