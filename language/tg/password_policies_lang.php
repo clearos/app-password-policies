461563a5941d7fd4c9b20411390fe6c5bc2997c5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:35:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'Ҳисоби Lockout';
+$lang['password_policies_app_description'] = 'Дар барнома сиёсати Рамз ба қоидаҳои сиёсати пароли корбари ҷӯр хуб истифода бурда мешавад. Ин таҳким нишонаҳо, ки ба меъёрњо ва кӯмак мерасонад, то ки системаи шумо эмин бештар.';
+$lang['password_policies_app_name'] = 'сиёсати Рамз';
+$lang['password_policies_history_size'] = 'Таърих Андоза';
+$lang['password_policies_maximum_password_age'] = 'Максимум Синну Рамз';
+$lang['password_policies_minimum_password_age'] = 'Хадди Синну Рамз';
+$lang['password_policies_minimum_password_length'] = 'Хадди Дарозии парол';
+$lang['password_policies_modify_any_time'] = 'Тағир гуна Time';
+$lang['password_policies_no_expire'] = 'Не эътибор';
+$lang['password_policies_no_history'] = 'Не Таърих';
