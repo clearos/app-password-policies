1e1f942737c55670749523cdd347823322c8d6a7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:29:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/it/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'blocco degli account';
+$lang['password_policies_app_description'] = 'L&#39;applicazione Criteri password viene utilizzato per le regole dei criteri password utente Sintonia fine. Questo rafforza le password che soddisfano i criteri e contribuisce a rendere il sistema più sicuro.';
+$lang['password_policies_app_name'] = 'Criteri password';
+$lang['password_policies_history_size'] = 'storia Size';
+$lang['password_policies_maximum_password_age'] = 'Massima password';
+$lang['password_policies_minimum_password_age'] = 'Minima password';
+$lang['password_policies_minimum_password_length'] = 'Lunghezza minima della password';
+$lang['password_policies_modify_any_time'] = 'Modificare Qualsiasi Tempo';
+$lang['password_policies_no_expire'] = 'No Scadenza';
+$lang['password_policies_no_history'] = 'No Storia';
