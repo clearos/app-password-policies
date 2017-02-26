01c4bf05cd0ce0733389ea83a9e11082c7be781c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:36:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'Account Lockout';
+$lang['password_policies_app_description'] = 'Awọn Ọrọigbaniwọle Afihan app wa ni lo lati itanran tune olumulo ọrọigbaniwọle imulo ofin. Eleyi enforces awọn ọrọigbaniwọle ti pade rẹ àwárí mu ati ki o iranlọwọ lati ṣe rẹ eto diẹ ni aabo.';
+$lang['password_policies_app_name'] = 'ọrọigbaniwọle imulo';
+$lang['password_policies_history_size'] = 'itan Iwon';
+$lang['password_policies_maximum_password_age'] = 'O pọju Ọrọigbaniwọle ori';
+$lang['password_policies_minimum_password_age'] = 'Kere Ọrọigbaniwọle ori';
+$lang['password_policies_minimum_password_length'] = 'Kere Ọrọigbaniwọle Ipari';
+$lang['password_policies_modify_any_time'] = 'Yipada Eyikeyi Time';
+$lang['password_policies_no_expire'] = 'ko si pari';
+$lang['password_policies_no_history'] = 'ko si itan';
