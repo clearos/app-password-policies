bdda73cd49188e4c4f0230df2a2ec92020fa5934
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:30:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'pūkete kaiwhakahaere';
+$lang['password_policies_app_description'] = 'Te taupānga Kaupapahere Kupuhipa te whakamahia ki ture kaupapa kupuhipa rangi kaiwhakamahi pai. whakaū ana tēnei kupuhipa e te whakatau koutou paearu me tauturu ki te hanga haumaru ake tou pūnaha.';
+$lang['password_policies_app_name'] = 'kaupapa here kupuhipa';
+$lang['password_policies_history_size'] = 'Rahi History';
+$lang['password_policies_maximum_password_age'] = 'Age Kupuhipa Mōrahi';
+$lang['password_policies_minimum_password_age'] = 'Age Kupuhipa iti';
+$lang['password_policies_minimum_password_length'] = 'Length Kupuhipa iti';
+$lang['password_policies_modify_any_time'] = 'Whakakē tetahi Wā';
+$lang['password_policies_no_expire'] = 'No hemo';
+$lang['password_policies_no_history'] = 'No History';
