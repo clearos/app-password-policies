d9cb261df811a48a6b1750b96152b65f7071bb95
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:34:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'akaunti Lockout';
+$lang['password_policies_app_description'] = 'programu Nywila Sera hutumika tune user sheria faini sera password. Hii utekelezaji nywila kwamba kufikia vigezo yako na husaidia kufanya mfumo wako salama zaidi.';
+$lang['password_policies_app_name'] = 'Sera nywila';
+$lang['password_policies_history_size'] = 'historia Size';
+$lang['password_policies_maximum_password_age'] = 'Upeo Nywila Umri';
+$lang['password_policies_minimum_password_age'] = 'Kiwango cha chini Nywila Umri';
+$lang['password_policies_minimum_password_length'] = 'Kiwango cha chini Nywila Length';
+$lang['password_policies_modify_any_time'] = 'Kurekebisha Muda yeyote';
+$lang['password_policies_no_expire'] = 'hakuna kumalizika';
+$lang['password_policies_no_history'] = 'hakuna Historia';
