93fb27bb1b84ca2422e452ff0c87d7d4915ab219
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:28:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'Account Lockout';
+$lang['password_policies_app_description'] = 'The Password Policy ngwa na-eji iji hụchaa ọrụ paswọọdụ iwu iwu. Nke a enforces okwuntughe na izute gị ibiere na aka eme ka gị usoro ọzọ ala.';
+$lang['password_policies_app_name'] = 'Password atumatu';
+$lang['password_policies_history_size'] = 'History Size';
+$lang['password_policies_maximum_password_age'] = 'Oke Password Age';
+$lang['password_policies_minimum_password_age'] = 'Kacha nta Password Age';
+$lang['password_policies_minimum_password_length'] = 'Kacha nta Password Ogologo';
+$lang['password_policies_modify_any_time'] = 'Gbanwee Ọ bụla Time';
+$lang['password_policies_no_expire'] = 'Ọ dịghị ekubi ume';
+$lang['password_policies_no_history'] = 'Ọ dịghị History';
