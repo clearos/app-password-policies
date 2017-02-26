6664971606d776f02ed1aa67126ce61a26311c68
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:35:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'اکاؤنٹ طالابندی';
+$lang['password_policies_app_description'] = 'پاس ورڈ کی پالیسی اپلی کیشن ٹھیک دھن صارف پاس ورڈ کی پالیسی قواعد کے استعمال کیا جاتا ہے. یہ آپ کے معیار کو پورا ہے کہ پاس ورڈز نافذ کرتا ہے اور آپ کے نظام کو زیادہ محفوظ بنانے کے لئے مدد کرتا ہے.';
+$lang['password_policies_app_name'] = 'پاس ورڈ کی پالیسیوں';
+$lang['password_policies_history_size'] = 'ہسٹری سائز';
+$lang['password_policies_maximum_password_age'] = 'زیادہ سے زیادہ پاس ورڈ عمر';
+$lang['password_policies_minimum_password_age'] = 'کم از کم کے پاس ورڈ عمر';
+$lang['password_policies_minimum_password_length'] = 'کم از کم کے پاس ورڈ کی لمبائی';
+$lang['password_policies_modify_any_time'] = 'کسی بھی وقت میں ترمیم کریں';
+$lang['password_policies_no_expire'] = 'کوئی میعاد ختم';
+$lang['password_policies_no_history'] = 'کوئی تاریخ';
