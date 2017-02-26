6c5916a984fc0f089b2d625a7c523cd22cd6fa7f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:26:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'konto Lockout';
+$lang['password_policies_app_description'] = 'Password Policy app bruges til at finjustere brugerens password retningslinjer. Dette håndhæver adgangskoder, der opfylder dine kriterier og hjælper med at gøre dit system mere sikkert.';
+$lang['password_policies_app_name'] = 'Password Politikker';
+$lang['password_policies_history_size'] = 'Historie Størrelse';
+$lang['password_policies_maximum_password_age'] = 'Maksimal Password Age';
+$lang['password_policies_minimum_password_age'] = 'Minimum Password Age';
+$lang['password_policies_minimum_password_length'] = 'Minimum Password Længde';
+$lang['password_policies_modify_any_time'] = 'Ændre Any Time';
+$lang['password_policies_no_expire'] = 'ingen Udløber';
+$lang['password_policies_no_history'] = 'ingen historie';
