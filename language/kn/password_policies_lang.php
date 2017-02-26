18d09e4862096dc25c09210bfb505265f19f1da3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:29:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'ಖಾತೆ ಲಾಕ್ಔಟ್';
+$lang['password_policies_app_description'] = 'ಪಾಸ್ವರ್ಡ್ ನೀತಿ ಅಪ್ಲಿಕೇಶನ್ ಉತ್ತಮ ಟ್ಯೂನ್ ಬಳಕೆದಾರ ಪಾಸ್ವರ್ಡ್ ಕಾರ್ಯನೀತಿಯ ನಿಯಮಗಳ ಬಳಸಲಾಗುತ್ತದೆ. ನಿಮ್ಮ ಮಾನದಂಡಗಳನ್ನು ಪಾಸ್ವರ್ಡ್ಗಳನ್ನು ನಿಗದಿಪಡಿಸುತ್ತದೆ ಮತ್ತು ನಿಮ್ಮ ವ್ಯವಸ್ಥೆಯ ಹೆಚ್ಚು ಸುರಕ್ಷಿತ ಮಾಡಲು ಸಹಾಯ ಮಾಡುತ್ತದೆ.';
+$lang['password_policies_app_name'] = 'ಪಾಸ್ವರ್ಡ್ ನೀತಿಗಳು';
+$lang['password_policies_history_size'] = 'ಇತಿಹಾಸ ಗಾತ್ರ';
+$lang['password_policies_maximum_password_age'] = 'ಗರಿಷ್ಠ ಪಾಸ್ವರ್ಡ್ ವಯಸ್ಸು';
+$lang['password_policies_minimum_password_age'] = 'ಕನಿಷ್ಠ ಪಾಸ್ವರ್ಡ್ ವಯಸ್ಸು';
+$lang['password_policies_minimum_password_length'] = 'ಕನಿಷ್ಠ ಪಾಸ್ವರ್ಡ್ ಉದ್ದ';
+$lang['password_policies_modify_any_time'] = 'ಯಾವುದಾದರೂ ಟೈಮ್ ಮಾರ್ಪಡಿಸಿ';
+$lang['password_policies_no_expire'] = 'ಯಾವುದೇ ಮುಕ್ತಾಯ';
+$lang['password_policies_no_history'] = 'ಯಾವುದೇ ಇತಿಹಾಸ';
