6268ad84c2f2fc26a42eb3f558949dd5b048e6d7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:25:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'kontu-';
+$lang['password_policies_app_description'] = 'Pasahitz politika Aplikazioa xehetasunez erabiltzailearen pasahitza politikaren arauak erabiltzen da. Hau zure irizpideak betetzen dituzten pasahitzak behartzean eta zure sistema seguruagoa izan dadin laguntzen du.';
+$lang['password_policies_app_name'] = 'Pasahitza Politikak';
+$lang['password_policies_history_size'] = 'Historia neurria';
+$lang['password_policies_maximum_password_age'] = 'Gehienezko Password Age';
+$lang['password_policies_minimum_password_age'] = 'Gutxieneko Password Age';
+$lang['password_policies_minimum_password_length'] = 'Gutxieneko Pasahitza Luzera';
+$lang['password_policies_modify_any_time'] = 'Aldatzeko ordua';
+$lang['password_policies_no_expire'] = 'Ez Iraungi';
+$lang['password_policies_no_history'] = 'Historia No';
