fff9edd1a2e3e9330a4627b6d5d3fcbe70dec6b9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:33:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/password_policies_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['password_policies_account_lockout'] = 'ගිණුම් වැළකුම';
+$lang['password_policies_app_description'] = 'මුරපද ප්රතිපත්ති යෙදුම සීඝ්රගාමී පරිශීලක මුරපද ප්රතිපත්තියකට නීති රීති භාවිතා කර ඇත. මෙය ඔබේ උපමානයට බව මුරපද අනුබල හා ඔබේ පද්ධතිය වඩාත් ආරක්ෂිත කිරීමට උපකාරී වේ.';
+$lang['password_policies_app_name'] = 'රහස් පදය ප්රතිපත්ති';
+$lang['password_policies_history_size'] = 'ඉතිහාසය තරම';
+$lang['password_policies_maximum_password_age'] = 'උපරිම මුරපදය වයස';
+$lang['password_policies_minimum_password_age'] = 'අවම මුරපදය වයස';
+$lang['password_policies_minimum_password_length'] = 'අවම මුරපදය දිග';
+$lang['password_policies_modify_any_time'] = 'ඕනෑම කාල වෙනස් කරන්න';
+$lang['password_policies_no_expire'] = 'කිසිදු කල් ඉකුත්';
+$lang['password_policies_no_history'] = 'කිසිදු ඉතිහාසය';
